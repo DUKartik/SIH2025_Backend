e7fa7deb7638565f5c614c273027221b3b69a913
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-05 17:22:59 +0530

    register student controller written

---
+++ src/models/student.model.js
@@ -0,0 +1,22 @@
+import {User} from "./baseUser.model.js"
+
+const StudentSchema = new mongoose.Schema({
+    college_roll: { 
+        type: String, 
+        required: true 
+    },
+    batch_year: { 
+        type: Number, 
+        required: true 
+    },
+    course: { 
+        type: String, 
+        required: true 
+    },
+    branch: { 
+        type: String, 
+        required: true 
+    },
+});
+
+export const Student = User.discriminator("Student", StudentSchema);

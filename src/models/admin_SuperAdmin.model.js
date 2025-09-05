d35c752b962deaac04d7a9095c797b7f87ff3ff8
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-05 17:35:46 +0530

    added few models and updated some js file

---
+++ src/models/admin_SuperAdmin.model.js
@@ -0,0 +1,71 @@
+import {User} from "./baseUser.model.js"
+
+const AdminSchema = new mongoose.Schema({
+  permissions: {
+    manage_users: { 
+        type: Boolean, 
+        default: true 
+    },
+    send_emails: { 
+        type: Boolean, 
+        default: true 
+    },
+    manage_events: { 
+        type: Boolean, 
+        default: true 
+    },
+    manage_donations: { 
+        type: Boolean, 
+        default: true 
+    },
+    view_audit_logs: { 
+        type: Boolean, 
+        default: true 
+    },
+  },
+});
+
+const Admin = User.discriminator("Admin", AdminSchema);
+
+
+const SuperAdminSchema = new mongoose.Schema({
+  permissions: {
+    manage_users: { 
+        type: Boolean, 
+        default: true 
+        
+    },
+    send_emails: { 
+        type: Boolean, 
+        default: true 
+        
+    },
+    manage_events: { 
+        type: Boolean, 
+        default: true 
+        
+    },
+    manage_donations: { 
+        type: Boolean, 
+        default: true 
+        
+    },
+    view_audit_logs: { 
+        type: Boolean, 
+        default: true 
+        
+    },
+    manage_admins: { 
+        type: Boolean, 
+        default: true 
+        
+    }, // super power
+  },
+});
+
+const SuperAdmin = User.discriminator("Super-admin", SuperAdminSchema);
+
+export {
+    Admin,
+    SuperAdmin
+}

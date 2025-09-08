5dea78cb6c771158fec6245398770ce0eb80ed87
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-08 20:49:49 +0530

    cookie sending bug fixed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",  // frontend URL
+  credentials: true,                // allow cookies (important!)
+}));
 
 app.use(express.json({limit:"16kb",}));
 app.use(express.urlencoded({limit:"16kb"}));

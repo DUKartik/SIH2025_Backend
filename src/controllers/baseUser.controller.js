6355b93136c94a0e57daf519ffcc3bd6137fedd2
Gaurav Rao <gaurav.swf@gmail.com>
2025-09-10 21:48:15 +0530

    Update baseUser.controller.js
    made connection settings suitable for connecting to frontend hosted separately

--- src/controllers/baseUser.controller.js
+++ src/controllers/baseUser.controller.js
@@ -52,8 +52,8 @@ const Login = asyncHandler(async (req,res)=>{
 
     const option={
         httpOnly:true,
-        secure:false, // set it to true after creating frontend
-        //sameSite: "none"      // remove the comment when deploying
+        secure:true, // set it to false when testing with postman
+        sameSite: "none"      // comment this line when testing with postman
     }
 
     return res

73fc1c4f458f38dc3802d38c8312fade7e12baf2
Kartik Nath Khatri <masterbro770@gmail.com>
2025-09-12 12:09:54 +0530

    All crud operations on events completed

--- src/routes/baseUser.route.js
+++ src/routes/baseUser.route.js
@@ -2,8 +2,11 @@ import { Router } from "express";
 import { Login,
         logout,
         refreshAccessToken,
-        changeCurrentPassword,} from "../controllers/baseUser.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+        changeCurrentPassword,
+        getAllEvents,
+        updateEventDetails,
+        deleteEvent} from "../controllers/baseUser.controller.js";
+import { verifyJWT,authorizeRoles } from "../middlewares/auth.middleware.js";
 
 const router =Router();
 
@@ -12,5 +15,9 @@ router.route("/login").post(Login);
 router.route("/logout").post(verifyJWT,logout);
 router.route("/refreshAccessToken").get(verifyJWT,refreshAccessToken);
 router.route("/ChangeCurrentPassword").patch(verifyJWT,changeCurrentPassword);
+
+router.route("/getAllEvents").get(verifyJWT,getAllEvents);
+router.route("/updateEventDetails").patch(verifyJWT,authorizeRoles("Admin"),updateEventDetails);
+router.route("/deleteEvent").delete(verifyJWT,authorizeRoles("Admin"),deleteEvent);
 export default router;
 

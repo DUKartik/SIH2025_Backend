bd0282b801cfee4fb7d72aa033abfcea005a826c
gauravstead <gaurav.swf@gmail.com>
2025-09-14 08:35:02 +0530

    Users can now search for other users and initiate chat and send messages in real time with history

--- src/routes/baseUser.route.js
+++ src/routes/baseUser.route.js
@@ -4,13 +4,17 @@ import { Login,
         refreshAccessToken,
         changeCurrentPassword,} from "../controllers/baseUser.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import { searchUsers, getUserById } from "../controllers/searchUser.controller.js";
 const router =Router();
 
 router.route("/login").post(Login);
 
 router.route("/logout").post(verifyJWT,logout);
-router.route("/refreshAccessToken").get(verifyJWT,refreshAccessToken);
+router.route("/refreshAccessToken").post(refreshAccessToken);
 router.route("/ChangeCurrentPassword").patch(verifyJWT,changeCurrentPassword);
+
+router.get("/search", verifyJWT, searchUsers);
+router.get("/profile/:id", verifyJWT, getUserById);
+
 export default router;
 
